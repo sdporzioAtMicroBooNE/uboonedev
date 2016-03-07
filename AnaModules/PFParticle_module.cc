3189de7d152e2ac37a50d571487744da599d8d64
Usher, Tracy L <usher@slac.stanford.edu>
2016-03-07 08:15:33 -0800

    PFParticle traversal module

---
+++ AnaModules/PFParticle_module.cc
@@ -0,0 +1,352 @@
+// PFParticle_module.cc
+// A basic "skeleton" to read in art::Event records from a file,
+// access their information, and do something with them. 
+
+// See
+// <https://cdcvs.fnal.gov/redmine/projects/larsoftsvn/wiki/Using_the_Framework>
+// for a description of the ART classes used here.
+
+// Almost everything you see in the code below may have to be changed
+// by you to suit your task. The example task is to make histograms
+// and n-tuples related to dE/dx of particle tracks in the detector.
+
+// As you try to understand why things are done a certain way in this
+// example ("What's all this stuff about 'auto const&'?"), it will help
+// to read ADDITIONAL_NOTES.txt in the same directory as this file.
+
+#ifndef PFParticle_module
+#define PFParticle_module
+
+// LArSoft includes
+#include "larcore/Geometry/Geometry.h"
+#include "larcore/SimpleTypesAndConstants/geo_types.h"
+#include "lardata/RawData/RawDigit.h"
+#include "lardata/RawData/raw.h"
+#include "lardata/RecoBase/Hit.h"
+#include "lardata/RecoBase/Cluster.h"
+#include "lardata/RecoBase/PFParticle.h"
+#include "lardata/RecoBase/Track.h"
+#include "lardata/RecoBase/Vertex.h"
+#include "lardata/RecoBase/Spacepoint.h"
+#include "larevt/CalibrationDBI/Interface/DetPedestalService.h"
+#include "larevt/CalibrationDBI/Interface/DetPedestalProvider.h"
+
+//#include "cetlib/search_path.h"
+#include "cetlib/cpu_timer.h"
+#include "lardata/Utilities/AssociationUtil.h"
+#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
+#include "lardata/DetectorInfoServices/LArPropertiesService.h"
+#include "lardata/DetectorInfoServices/DetectorClocksService.h"
+
+// Framework includes
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/View.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/FindManyP.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "cetlib/exception.h"
+
+// ROOT includes. Note: To look up the properties of the ROOT classes,
+// use the ROOT web site; e.g.,
+// <http://root.cern.ch/root/html532/ClassIndex.html>
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
+#include "TTree.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+
+#include "TVirtualFFT.h"
+
+// C++ Includes
+#include <map>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#include <iostream>
+#include <fstream>
+
+namespace PFParticle
+{
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+// class definition
+
+class PFParticle : public art::EDAnalyzer
+{
+public:
+ 
+    // Standard constructor and destructor for an ART module.
+    explicit PFParticle(fhicl::ParameterSet const& pset);
+    virtual ~PFParticle();
+
+    // This method is called once, at the start of the job. In this
+    // example, it will define the histograms and n-tuples we'll write.
+    void beginJob();
+    void endJob();
+
+    // This method is called once, at the start of each run. It's a
+    // good place to read databases or files that may have
+    // run-dependent information.
+    void beginRun(const art::Run& run);
+
+    // This method reads in any parameters from the .fcl files. This
+    // method is called 'reconfigure' because it might be called in the
+    // middle of a job; e.g., if the user changes parameter values in an
+    // interactive event display.
+    void reconfigure(fhicl::ParameterSet const& pset);
+
+    // The analysis routine, called once per event. 
+    void analyze (const art::Event& evt); 
+
+private:
+    
+    // Traverse PFParticle hierarchy
+    int traversePFParticleHierarchy(art::Handle<std::vector<recob::PFParticle>>&,
+                                    size_t,
+                                    const art::FindManyP<recob::Track>&,
+                                    const art::FindManyP<recob::Vertex>&,
+                                    int&,
+                                    int&) const;
+    
+    // The following typedefs will, obviously, be useful
+    using  HitPtrVec = std::vector<art::Ptr<recob::Hit>>;
+    
+    double length(const recob::Track* track);
+    
+    double projectedLength(const recob::Track* track);
+
+    // The parameters we'll read from the .fcl file.
+    std::string fHitProducerLabel;
+    std::string fPFParticleProducerLabel;
+    std::string fTrackProducerLabel;
+    std::string fWireProducerLabel;
+    
+    // The variables that will go into the n-tuple.
+    int fEvent;
+    int fRun;
+    int fSubRun;
+    int fNumEvents;
+    
+    std::vector<std::vector<double>> fChannelPedVec;
+
+    // Other variables that will be shared between different methods.
+    const geo::GeometryCore*           fGeometry;       // pointer to Geometry service
+    const detinfo::DetectorProperties* fDetectorProperties;
+    const lariov::DetPedestalProvider& fPedestalRetrievalAlg; ///< Keep track of an instance to the pedestal retrieval alg
+}; // class PFParticle
+
+
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+// class implementation
+
+//-----------------------------------------------------------------------
+// Constructor
+PFParticle::PFParticle(fhicl::ParameterSet const& parameterSet)
+    : EDAnalyzer(parameterSet),
+      fPedestalRetrievalAlg(*lar::providerFrom<lariov::DetPedestalService>())
+
+{
+    fGeometry = lar::providerFrom<geo::Geometry>();
+    fDetectorProperties = lar::providerFrom<detinfo::DetectorPropertiesService>();
+    
+    // Read in the parameters from the .fcl file.
+    this->reconfigure(parameterSet);
+}
+
+//-----------------------------------------------------------------------
+// Destructor
+PFParticle::~PFParticle()
+{}
+   
+//-----------------------------------------------------------------------
+void PFParticle::beginJob()
+{
+    // Get the detector length, to determine the maximum bin edge of one
+    // of the histograms.
+    //double detectorLength = fGeometry->DetLength();
+
+    // Access ART's TFileService, which will handle creating and writing
+    // histograms and n-tuples for us. 
+    art::ServiceHandle<art::TFileService> tfs;
+    
+    // zero out the event counter
+    fNumEvents = 0;
+}
+   
+//-----------------------------------------------------------------------
+void PFParticle::beginRun(const art::Run& /*run*/)
+{
+    // How to convert from number of electrons to GeV.  The ultimate
+    // source of this conversion factor is
+    // ${LARSIM_DIR}/include/SimpleTypesAndConstants/PhysicalConstants.h.
+//    art::ServiceHandle<sim::LArG4Parameters> larParameters;
+//    fElectronsToGeV = 1./larParameters->GeVToElectrons();
+}
+
+//-----------------------------------------------------------------------
+void PFParticle::reconfigure(fhicl::ParameterSet const& p)
+{
+    // Read parameters from the .fcl file. The names in the arguments
+    // to p.get<TYPE> must match names in the .fcl file.
+    fHitProducerLabel        = p.get< std::string >("HitModuleLabel",          "gauss");
+    fPFParticleProducerLabel = p.get< std::string >("PFParticleProducerLabel", "cluster3d");
+    fTrackProducerLabel      = p.get< std::string >("TrackProducerLabel",      "trackkalmanhit");
+    fWireProducerLabel       = p.get< std::string >("WireProducerLabel",       "caldata");
+
+    return;
+}
+
+//-----------------------------------------------------------------------
+void PFParticle::analyze(const art::Event& event)
+{
+    // Start by fetching some basic event information for our n-tuple.
+    fEvent  = event.id().event(); 
+    fRun    = event.run();
+    fSubRun = event.subRun();
+    
+    fNumEvents++;
+    
+    // The game plan for this module is to look at hits associated to tracks
+    // To do this we need a valid track collection for those we are hoping to look at
+    art::Handle<std::vector<recob::PFParticle> > pfParticleHandle;
+    event.getByLabel(fPFParticleProducerLabel, pfParticleHandle);
+    
+    if (pfParticleHandle.isValid())
+    {
+        // Recover the collection of associations between PFParticles and Tracks from the PFParticle producer
+        art::FindManyP<recob::Track> pfTrackAssns(pfParticleHandle, event, fPFParticleProducerLabel);
+        
+        // Recover the collection of associations between PFParticles and vertices from the PFParticle producer
+        art::FindManyP<recob::Vertex> vertexAssns(pfParticleHandle, event, fPFParticleProducerLabel);
+        
+        for(size_t pfPartIdx = 0; pfPartIdx < pfParticleHandle->size(); pfPartIdx++)
+        {
+            art::Ptr<recob::PFParticle> pfPart(pfParticleHandle,pfPartIdx);
+            
+            if (pfPart->Parent() == recob::PFParticle::kPFParticlePrimary)
+            {
+                int nTracks(0);
+                int nVertices(0);
+                
+                traversePFParticleHierarchy(pfParticleHandle, pfPartIdx, pfTrackAssns, vertexAssns, nTracks, nVertices);
+                
+                std::cout << "************ " << nTracks << ", nVertices: " << nVertices << std::endl;
+            }
+        }
+    }
+
+    return;
+}
+    
+void PFParticle::endJob()
+{
+    // Make a call to normalize histograms if so desired
+    
+    return;
+}
+    
+// Length of reconstructed track.
+//----------------------------------------------------------------------------
+double PFParticle::length(const recob::Track* track)
+{
+    double   result(0.);
+    TVector3 disp(track->LocationAtPoint(0));
+    TVector3 lastPoint(track->LocationAtPoint(0));
+    TVector3 lastDir(0.,0.,0.);
+    int      n(track->NumberTrajectoryPoints());
+    
+    for(int i = 1; i < n; ++i)
+    {
+        const TVector3& pos = track->LocationAtPoint(i);
+        
+        TVector3 trajDir = pos - lastPoint;
+        
+        if (trajDir.Mag2()) trajDir.SetMag(1.);
+        
+//        if (lastDir.Dot(trajDir) >= 0.)
+//        {
+            disp   -= pos;
+            result += disp.Mag();
+            disp    = pos;
+//        }
+        
+        lastPoint = pos;
+        lastDir   = trajDir;
+    }
+    
+    return result;
+}
+
+// Length of reconstructed track.
+//----------------------------------------------------------------------------
+double PFParticle::projectedLength(const recob::Track* track)
+{
+    double   result(0.);
+    TVector3 lastPoint(track->LocationAtPoint(0));
+    TVector3 lastDir(track->DirectionAtPoint(0));
+    int      n(track->NumberTrajectoryPoints());
+    
+    for(int i = 1; i < n; ++i)
+    {
+        const TVector3& newPoint = track->LocationAtPoint(i);
+        
+        TVector3 lastToNewPoint = newPoint - lastPoint;
+        double   arcLenToDoca   = lastDir.Dot(lastToNewPoint);
+        
+        result    += arcLenToDoca;
+        lastPoint  = lastPoint + arcLenToDoca * lastDir;
+        lastDir    = track->DirectionAtPoint(i);
+    }
+    
+    return result;
+}
+
+int PFParticle::traversePFParticleHierarchy(art::Handle<std::vector<recob::PFParticle>>& pfParticleHandle,
+                                             size_t                                      pfParticleIdx,
+                                             const art::FindManyP<recob::Track>&         trackAssns,
+                                             const art::FindManyP<recob::Vertex>&        vertexAssns,
+                                             int&                                        nTracks,
+                                             int&                                        nVertices) const
+{
+    // So far no daughters...
+    int nDaughters(0);
+    
+    // Get pointer to PFParticle
+    art::Ptr<recob::PFParticle> pfParticle(pfParticleHandle,pfParticleIdx);
+    
+    std::cout << "-- Parent: " << pfParticle->Parent() << ", pdg code: " << pfParticle->PdgCode() << ", # daughters: " << pfParticle->NumDaughters() << std::endl;
+    
+    // Recover tracks/vertices associated to this PFParticle
+    std::vector<art::Ptr<recob::Track>>  pfPartTrackVec  = trackAssns.at(pfParticle.key());
+    std::vector<art::Ptr<recob::Vertex>> pfPartVertexVec = vertexAssns.at(pfParticle.key());
+
+    nTracks    += pfPartTrackVec.size();
+    nVertices  += pfPartVertexVec.size();
+    nDaughters += pfParticle->Daughters().size();
+    
+    for(auto& daughterIdx : pfParticle->Daughters())
+    {
+        nDaughters += traversePFParticleHierarchy(pfParticleHandle, daughterIdx, trackAssns, vertexAssns, nTracks, nVertices);
+    }
+    
+    return nDaughters;
+}
+
+// This macro has to be defined for this module to be invoked from a
+// .fcl file; see PFParticle.fcl for more information.
+DEFINE_ART_MODULE(PFParticle)
+
+} // namespace PFParticle
+
+#endif // PFParticle_module
